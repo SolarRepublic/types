cadfc81876b30607633362d722c9c629f3d9de71
Blake Regalia <blake.regalia@gmail.com>
2024-03-13 19:43:52 -0700

    fix: trsuted context urls

--- src/generic.ts
+++ src/generic.ts
@@ -1,6 +1,16 @@
 
-export type LocalhostUrl = `http://localhost${`:${bigint}` | ''}${`/${string}` | ''}`;
+type Subnet16 = `.${bigint}.${bigint}`;
+type Subnet8 = `.${bigint}${Subnet16}`;
+type OptionalPath = `${`/${string}` | ''}`;
+
+type AnyPortAndPath = `${`:${bigint}` | ''}${OptionalPath}`;
+
+export type LocalhostUrl = `http://${'localhost' | `127${Subnet8}`}${AnyPortAndPath}`;
+
+export type NetworkedUrl = `http://10${Subnet8}${AnyPortAndPath}`
+	| `http://192.168${Subnet16}${AnyPortAndPath}`
+	| `http://192.0.0.${bigint}${AnyPortAndPath}`;
 
 export type HttpsUrl = `https://${string}`;
 
-export type TrustedContextUrl = HttpsUrl | LocalhostUrl;
+export type TrustedContextUrl = HttpsUrl | LocalhostUrl | NetworkedUrl;
